a5cac5a16c7defebcdc1e1d3086e478809d70a5d
Matthieu Gautier <mgautier@kymeria.fr>
2023-01-09 10:04:58 +0100

    Introduce a auto_mount binary.
    This binary is not intended to be directly use.
    However, it can be concatened with a arx archive at its end.
    It creates a auto mountable archive.

---
+++ src/bin/auto_mount.rs
@@ -0,0 +1,47 @@
+use jubako as jbk;
+
+use clap::Parser;
+use std::env;
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+#[derive(Parser)]
+#[clap(name = "arx")]
+#[clap(author, version, about, long_about=None)]
+struct Cli {
+    #[clap(short, long, action=clap::ArgAction::Count)]
+    verbose: u8,
+
+    #[clap(value_parser)]
+    mountdir: PathBuf,
+}
+
+fn main() -> ExitCode {
+    let args = Cli::parse();
+
+    match env::current_exe() {
+        Ok(exe_path) => {
+            if args.verbose > 0 {
+                println!("Auto Mount archive {:?} in {:?}", exe_path, args.mountdir);
+            }
+            match arx::mount(exe_path, args.mountdir) {
+                Ok(()) => ExitCode::SUCCESS,
+                Err(e) => match e.error {
+                    jbk::ErrorKind::NotAJbk => {
+                        eprintln!("Impossible to locate a Jubako archive in the executable.");
+                        eprintln!("This binary is not intented to be directly used, you must put a Jubako archive at its end.");
+                        ExitCode::FAILURE
+                    }
+                    _ => {
+                        eprintln!("Error: {}", e);
+                        ExitCode::FAILURE
+                    }
+                },
+            }
+        }
+        Err(e) => {
+            eprintln!("failed to get current exe path: {e}");
+            ExitCode::FAILURE
+        }
+    }
+}
