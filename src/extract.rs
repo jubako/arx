4c8509eeff7de3cc58d2ac7bb848a65a831c4b01
Matthieu Gautier <mgautier@kymeria.fr>
2023-03-07 09:21:08 +0100

    Avoid duplication of data when writting file.
    Basic io use a temporary buffer in which the data is read from the `Read`
    and then pass to `Write`.
    
    But we already have a buffer in our Reader, so let's pass directly our
    buffer to `Write`.

--- src/extract.rs
+++ src/extract.rs
@@ -1,6 +1,7 @@
 use crate::common::*;
 use jubako as jbk;
 use std::fs::{create_dir, create_dir_all, File};
+use std::io::Write;
 use std::os::unix::fs::symlink;
 use std::path::{Path, PathBuf};
 
@@ -28,7 +29,19 @@ impl<'a> ArxOperator for Extractor<'a> {
         let reader = self.container.get_reader(entry.get_content_address())?;
         current_path.push(entry.get_path()?);
         let mut file = File::create(&current_path)?;
-        std::io::copy(&mut reader.create_flux_all(), &mut file)?;
+        let size = reader.size().into_usize();
+        let mut offset = 0;
+        loop {
+            let sub_size = std::cmp::min(size - offset, 4 * 1024);
+            let reader = reader
+                .create_sub_memory_reader(offset.into(), jbk::End::new_size(sub_size))?
+                .into_memory_reader()?;
+            let written = file.write(reader.get_slice(jbk::Offset::zero(), jbk::End::None)?)?;
+            offset += written;
+            if offset == size {
+                break;
+            }
+        }
         current_path.pop();
         Ok(())
     }
