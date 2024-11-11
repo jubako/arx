41f0dc7848a13d57826450cc462a710d65fe5d0a
Matthieu Gautier <mgautier@kymeria.fr>
2024-11-11 12:16:22 +0100

    Introduce `detect_kind` helper to detect kind of file/path to add.

--- libarx/src/create/fs_adder.rs
+++ libarx/src/create/fs_adder.rs
@@ -1,23 +1,32 @@
 use crate::create::{EntryKind, EntryTrait, SimpleCreator, Void};
 use bstr::{BString, ByteVec};
 use jbk::creator::InputReader;
+use std::fs::Metadata;
 #[cfg(unix)]
 use std::os::unix::fs::MetadataExt;
 #[cfg(windows)]
 use std::os::windows::fs::MetadataExt;
 use std::path::PathBuf;
-use std::{fs, io::Cursor, sync::mpsc, thread::spawn};
+use std::{io::Cursor, sync::mpsc, thread::spawn};
 
+#[derive(Debug)]
 pub enum FsEntryKind {
     Dir,
     File(jbk::Size, jbk::ContentAddress),
-    Link,
+    Link(PathBuf),
+    Other,
+}
+
+#[derive(Debug)]
+pub enum DetectedEntryKind {
+    Dir,
+    File(u64, PathBuf),
+    Link(PathBuf),
     Other,
 }
 
 pub struct FsEntry {
     pub kind: FsEntryKind,
-    pub fs_path: PathBuf,
     pub arx_path: crate::PathBuf,
     uid: u64,
     gid: u64,
@@ -25,46 +34,58 @@ pub struct FsEntry {
     mtime: u64,
 }
 
+fn detect_kind(
+    mut path: PathBuf,
+    follow_symlink: bool,
+) -> jbk::Result<(DetectedEntryKind, Metadata)> {
+    log::trace!("std::fs::symlink_metadata({path:?})");
+    let mut attr = std::fs::symlink_metadata(&path)?;
+    log::trace!("=> {attr:?}");
+
+    if attr.is_symlink() && follow_symlink {
+        path = std::fs::canonicalize(path)?;
+        log::trace!("New path is {path:?}");
+        attr = std::fs::symlink_metadata(&path)?;
+        log::trace!("New attr is {attr:?}");
+    }
+    let kind = if attr.is_dir() {
+        DetectedEntryKind::Dir
+    } else if attr.is_file() {
+        DetectedEntryKind::File(attr.len(), path)
+    } else if attr.is_symlink() {
+        DetectedEntryKind::Link(path)
+    } else {
+        DetectedEntryKind::Other
+    };
+    Ok((kind, attr))
+}
+
 impl FsEntry {
-    pub fn new_from_walk_entry<A: jbk::creator::ContentAdder>(
-        dir_entry: walkdir::DirEntry,
+    pub fn new_from_path<A: jbk::creator::ContentAdder>(
+        fs_path: &std::path::Path,
         arx_path: crate::PathBuf,
         adder: &mut A,
-        is_root_entry: bool,
+        follow_symlink: bool,
     ) -> jbk::Result<Box<Self>> {
-        let fs_path = dir_entry.path().to_path_buf();
-        let attr = dir_entry.metadata().map_err(std::io::Error::from)?;
-        let kind = if attr.is_dir() {
-            FsEntryKind::Dir
-        } else if attr.is_file() {
-            let reader: Box<dyn InputReader> = if attr.len() < 1024 * 1024 {
-                let content = std::fs::read(&fs_path)?;
-                Box::new(Cursor::new(content))
-            } else {
-                Box::new(jbk::creator::InputFile::open(&fs_path)?)
-            };
-            let content_address = adder.add_content(reader, jbk::creator::CompHint::Detect)?;
-            FsEntryKind::File(attr.len().into(), content_address)
-        } else if attr.is_symlink() {
-            if is_root_entry {
-                // Walkdir will return it is a link but will follow it if it is a directory.
-                let target_attr = std::fs::metadata(std::fs::read_link(dir_entry.path())?)?;
-                if target_attr.is_dir() {
-                    FsEntryKind::Dir
+        let (kind, attr) = detect_kind(fs_path.to_path_buf(), follow_symlink)?;
+        let kind = match kind {
+            DetectedEntryKind::Dir => FsEntryKind::Dir,
+            DetectedEntryKind::File(file_size, path) => {
+                let reader: Box<dyn InputReader> = if file_size < 1024 * 1024 {
+                    let content = std::fs::read(&path)?;
+                    Box::new(Cursor::new(content))
                 } else {
-                    // Walkdir follow root link only if points to a directory.
-                    // So if it is not a directory, it is a link (no need to handle as a file)
-                    FsEntryKind::Link
-                }
-            } else {
-                FsEntryKind::Link
+                    Box::new(jbk::creator::InputFile::open(&path)?)
+                };
+                let content_address = adder.add_content(reader, jbk::creator::CompHint::Detect)?;
+                FsEntryKind::File(file_size.into(), content_address)
             }
-        } else {
-            FsEntryKind::Other
+            DetectedEntryKind::Link(path) => FsEntryKind::Link(std::fs::read_link(&path)?),
+            DetectedEntryKind::Other => FsEntryKind::Other,
         };
+        log::debug!("{fs_path:?} is dectected as a {kind:?}");
         Ok(Box::new(Self {
             kind,
-            fs_path,
             arx_path,
             #[cfg(unix)]
             uid: attr.uid() as u64,
@@ -89,19 +110,16 @@ impl FsEntry {
 
 impl EntryTrait for FsEntry {
     fn kind(&self) -> jbk::Result<Option<EntryKind>> {
-        Ok(match self.kind {
+        Ok(match &self.kind {
             FsEntryKind::Dir => Some(EntryKind::Dir),
             FsEntryKind::File(size, content_address) => {
-                Some(EntryKind::File(size, content_address))
+                Some(EntryKind::File(*size, *content_address))
             }
 
-            FsEntryKind::Link => {
-                let target = fs::read_link(&self.fs_path)?;
-                Some(EntryKind::Link(BString::from(
-                    Vec::from_path_buf(target)
-                        .unwrap_or_else(|target| panic!("{target:?} must be utf-8")),
-                )))
-            }
+            FsEntryKind::Link(target) => Some(EntryKind::Link(BString::from(
+                Vec::from_path_buf(target.clone())
+                    .unwrap_or_else(|target| panic!("{target:?} must be utf-8")),
+            ))),
             _ => None,
         })
     }
@@ -149,12 +167,15 @@ impl<'a> FsAdder<'a> {
         F: FnMut(&walkdir::DirEntry) -> bool,
         F: Send + 'static,
     {
+        let path = path.as_ref();
+        log::trace!("add_from_path_with_filter(path:{path:?}, recurse:{recurse})");
         let (tx, rx) = mpsc::channel();
-        let path_copy = path.as_ref().to_path_buf();
+        let path_copy = path.to_path_buf();
         let strip_prefix = self.strip_prefix.clone();
 
         spawn(move || {
             let mut walker = walkdir::WalkDir::new(path_copy);
+
             if !recurse {
                 walker = walker.max_depth(0);
             }
@@ -167,7 +188,7 @@ impl<'a> FsAdder<'a> {
 
         while let Ok(entry) = rx.recv() {
             // Walkdir behaves differently if root is a link to a directory
-            let is_root_entry = entry.path() == path.as_ref();
+            let is_root_entry = entry.path() == path;
 
             let entry_path = entry.path();
             let arx_path = match crate::PathBuf::from_path(entry_path) {
@@ -202,8 +223,12 @@ impl<'a> FsAdder<'a> {
                 continue;
             }
 
-            let entry =
-                FsEntry::new_from_walk_entry(entry, arx_path, self.creator.adder(), is_root_entry)?;
+            let entry = FsEntry::new_from_path(
+                entry.path(),
+                arx_path,
+                self.creator.adder(),
+                is_root_entry,
+            )?;
 
             self.creator.add_entry(entry.as_ref())?;
         }
